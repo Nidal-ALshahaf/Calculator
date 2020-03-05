feb72a76c812ac14567caed4fc7656268b5002d5
root <nidallama2015@hotmail.com>
2020-03-05 13:55:31 +0100

    Updates

---
+++ precache-manifest.500956699b34b62581b61cad75c97c70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7e9ac3cfeb3c991e2206259a022dcc",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "0e2bdfc468ab4a2facda",
+    "url": "/calculator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ba0f3c7ff3d1ac2729da",
+    "url": "/calculator/static/js/2.6ef9a01f.chunk.js"
+  },
+  {
+    "revision": "0e2bdfc468ab4a2facda",
+    "url": "/calculator/static/js/main.83bd95cd.chunk.js"
+  },
+  {
+    "revision": "f86523323069d7ee24e0",
+    "url": "/calculator/static/js/runtime-main.5e8ef56b.js"
+  }
+]);
